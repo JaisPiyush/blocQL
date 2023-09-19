e821f561520c64b7cb33cc1866a25048b7d5be94
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-09-19 12:24:56 +0530

    feat(consumer): added SQSConsumer

---
+++ packages/consumer/src/index.js
@@ -0,0 +1,123 @@
+import { Consumer } from 'sqs-consumer';
+import { SQSClient } from '@aws-sdk/client-sqs';
+
+export class SQSConsumer {
+
+    /** 
+     * @private
+     * @constant
+     * @type {string}
+     * 
+    **/
+    AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID';
+    /** 
+     * @private
+     * @constant
+     * @type {string}
+     * 
+    **/
+    AWS_REGION = 'AWS_REGION';
+    /** 
+     * @private
+     * @constant
+     * @type {string}
+     * 
+    **/
+    AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
+
+    // @property {SQSClient} sqs
+    sqs;
+
+    // @property {Consumer} app
+    app;
+
+    /**
+     * @async
+     * @callback handleMessageCallback
+     * @param {any} message
+     * 
+     * @callback consumerErrorCallback
+     * @param {object} err
+     * 
+     * @typedef {Object} SQSConsumerErrorOptions
+     *  @property {consumerErrorCallback} [error]
+     *  @property {consumerErrorCallback} [processingError]
+     *  @property {consumerErrorCallback} [timeoutError]
+     * 
+     * @typedef {Object} SQSConsumerOptions
+     *  @property {string} queueUrl
+     *  @property {handleMessageCallback} handleMessage
+     *  @property {string} [region]
+     *  @property {string} [accessKeyId]
+     *  @property {string} [secretAccessKey]
+     *  @property {number} [pollingWaitTimeMs]
+     *  @property {SQSConsumerErrorOptions} [error]
+     *  
+     * @param {SQSConsumerOptions} data
+     * 
+     * @throws {Error} AWS_REGION is required
+     * @throws {Error} AWS_ACCESS_KEY_ID is required
+     * @throws {Error} AWS_SECRET_ACCESS_KEY is required
+     */
+    constructor(data) {
+        let _queueUrl = data.queueUrl;
+        let _handleMessage = data.handleMessage;
+        let _region = data.region || process.env[this.AWS_REGION];
+        let _accessKeyId = data.accessKeyId || process.env[this.AWS_ACCESS_KEY_ID];
+        let _secretAccessKey = data.secretAccessKey || process.env[this.AWS_SECRET_ACCESS_KEY];
+        let _pollingWaitTimeMs = data.pollingWaitTimeMs || 3000;
+        if (!_region) throw new Error('AWS_REGION is required')
+        if (!_accessKeyId) throw new Error('AWS_ACCESS_KEY_ID is required')
+        if (!_secretAccessKey) throw new Error('AWS_SECRET_ACCESS_KEY is required')
+        this.sqs = new SQSClient({ region: _region, credentials: { accessKeyId: _accessKeyId, secretAccessKey: _secretAccessKey } });
+        this.app = Consumer.create({
+            queueUrl: _queueUrl,
+            handleMessage: _handleMessage,
+            sqs: this.sqs,
+            pollingWaitTimeMs: _pollingWaitTimeMs
+        });
+
+        let logCallback = (err) => {console.log(err)}
+
+        let _error = data.error || {};
+        this.app.on('error', _error.error || logCallback);
+        this.app.on('processing_error', _error.processingError || logCallback);
+        this.app.on('timeout_error', _error.timeoutError || logCallback);
+    }
+
+
+    /**
+     * Start the SQS consumer
+     * @method
+     */
+    start() {
+        if (this.app.isRunning()) return;
+        this.app.start();
+    }
+
+    // @property {boolean} isRunning
+    get isRunning() {
+        return this.app.isRunning();
+    }
+
+    /**
+     * Stop the SQS consumer
+     * @method
+     * @param {boolean} [abort=true] - default true
+    **/
+    stop(abort = true) {
+        if (!this.app.isRunning()) return;
+        this.app.stop({abort});
+    }
+    
+    /**
+     * @method
+     * @param {string} event 
+     * @param {callback} callback 
+     */
+    on(event, callback) {
+        this.app.on(event, callback);
+    }
+
+
+}
