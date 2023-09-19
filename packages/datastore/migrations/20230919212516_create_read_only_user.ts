3919a96f0560498019e7b86ca37da99e1d131a79
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-09-20 03:03:22 +0530

    feat(datastore): added docker and migrations

---
+++ packages/datastore/migrations/20230919212516_create_read_only_user.ts
@@ -0,0 +1,50 @@
+import { Knex } from "knex";
+
+const PG_READ_ONLY_USER = process.env['POSTGRES_READ_USER'];
+const PG_READ_ONLY_PASSWORD = process.env['POSTGRES_READ_PASSWORD'];
+const POSTGRES_DB = process.env['POSTGRES_DATABASE'];
+
+
+export async function up(knex: Knex): Promise<void> {
+    if (!PG_READ_ONLY_USER && !PG_READ_ONLY_PASSWORD && !POSTGRES_DB) {
+        throw new Error(
+            `Username: ${PG_READ_ONLY_USER} and password: ${PG_READ_ONLY_PASSWORD} not correctly defined.`
+        );
+    }
+
+    // Create ROLE
+    await knex.raw(`create role read_access`);
+    // Grant connect to ROLE
+    await knex.raw(`create connect on database ${POSTGRES_DB} to read_access`);
+    // Grant usage on schema to ROLE
+    await knex.raw(`grant usage on schema public to read_access`);
+    // Grant select on all tables to ROLE
+    await knex.raw(`grant select on all tables in schema public to read_access`);
+    // Create user
+    await knex.raw(`create user ${PG_READ_ONLY_USER} with encrypted password '${PG_READ_ONLY_PASSWORD}'`);
+    // Grant ROLE to user
+    await knex.raw(`grant read_access to ${PG_READ_ONLY_USER}`);
+
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    if (!PG_READ_ONLY_USER && !PG_READ_ONLY_PASSWORD && !POSTGRES_DB) {
+        throw new Error(
+            `Username: ${PG_READ_ONLY_USER} and password: ${PG_READ_ONLY_PASSWORD} not correctly defined.`
+        );
+    }
+
+    // REVOKE role
+    await knex.raw(`revoke read_access from ${PG_READ_ONLY_USER}`);
+    // drop user
+    await knex.raw(`drop user ${PG_READ_ONLY_USER}`);
+    // revoke usage on schema to role
+    await knex.raw(`revoke usage on schema public from read_access`);
+    // revoke connect on database to role
+    await knex.raw(`revoke connect on database ${POSTGRES_DB} from read_access`);
+    // drop role
+    await knex.raw(`drop role read_access`);
+
+}
+
---
+++ packages/datastore/src/constants.ts
@@ -0,0 +1,13 @@
+export enum TableNames {
+    ProcessedBlock = "processed_block",
+    SolanaBlock = "solana_block",
+    SolanaTransactions = "solana_transactions",
+    SolanaVoteTransactions = "solana_vote_transactions",
+    SolanaAccountActivity = "solana_account_activity",
+    SolanaTokensMetadata = "solana_tokens_metadata",
+    SolanaRewards = "solana_rewards",
+    SolanaInstructionCalls = "solana_instruction_calls"
+
+}
+
+export const PROCESSED_BLOCK_KEY_NAME = 'processed_block_key'
